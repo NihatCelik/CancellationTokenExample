cc56677c75a833aac189b9de885cf9f5bb0e746f
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-02-28 21:14:44 +0300

    ThrowIfCancellationRequested project added

---
+++ CancellationTokenThrowIfCancellationRequest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace CancellationTokenThrowIfCancellationRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            for (int i = 1; true; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (i % 100 == 0) cancellationTokenSource.Cancel();
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
